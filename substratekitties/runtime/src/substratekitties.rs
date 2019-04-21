6e97e75e728a4d09aafc80e163d2863c386ef559
vimukthi wickramasinghe <vimukthi@centrifuge.io>
2019-04-21 16:49:20 +0200

    Setting a price

--- substratekitties/runtime/src/substratekitties.rs
+++ substratekitties/runtime/src/substratekitties.rs
@@ -1,7 +1,8 @@
-use support::{decl_storage, decl_module, decl_event, dispatch::Result, StorageMap, StorageValue, ensure};
-use parity_codec::{Encode, Decode};
+use support::{decl_storage, decl_module, StorageValue, StorageMap,
+    dispatch::Result, ensure, decl_event};
 use system::ensure_signed;
 use runtime_primitives::traits::{As, Hash};
+use parity_codec::{Encode, Decode};
 
 #[derive(Encode, Decode, Default, Clone, PartialEq)]
 #[cfg_attr(feature = "std", derive(Debug))]
@@ -20,23 +21,25 @@ decl_event!(
     pub enum Event<T>
     where
         <T as system::Trait>::AccountId,
-        <T as system::Trait>::Hash
+        <T as system::Trait>::Hash,
+        <T as balances::Trait>::Balance,
     {
-        CreatedEvent(AccountId, Hash),
+        Created(AccountId, Hash),
+        PriceSet(AccountId, Hash, Balance),
     }
 );
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
         Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>;
-        KittyOwner get(owner_of_kitty): map T::Hash => T::AccountId;
+        KittyOwner get(owner_of): map T::Hash => Option<T::AccountId>;
 
         AllKittiesArray get(kitty_by_index): map u64 => T::Hash;
         AllKittiesCount get(all_kitties_count): u64;
         AllKittiesIndex: map T::Hash => u64;
 
         OwnedKittiesArray get(kitty_of_owner_by_index): map (T::AccountId, u64) => T::Hash;
-        OwnedKittiesCount get(kitties_count_by_owner): map T::AccountId => u64;
+        OwnedKittiesCount get(owned_kitty_count): map T::AccountId => u64;
         OwnedKittiesIndex: map T::Hash => u64;
 
         Nonce: u64;
@@ -50,25 +53,35 @@ decl_module! {
 
         fn create_kitty(origin) -> Result {
             let sender = ensure_signed(origin)?;
-
-            // `nonce` and `random_hash` generation can stay here
             let nonce = <Nonce<T>>::get();
             let random_hash = (<system::Module<T>>::random_seed(), &sender, nonce)
                 .using_encoded(<T as system::Trait>::Hashing::hash);
 
-            // Creating the `Kitty` object can stay here
             let new_kitty = Kitty {
                 id: random_hash,
                 dna: random_hash,
                 price: <T::Balance as As<u64>>::sa(0),
                 gen: 0,
             };
-            
-            // Nonce update can stay here
-            <Nonce<T>>::mutate(|n| *n += 1);
 
             Self::mint(sender, random_hash, new_kitty)?;
 
+            <Nonce<T>>::mutate(|n| *n += 1);
+
+            Ok(())
+        }
+
+        fn set_price(origin, kitty_id: T::Hash, new_price: T::Balance) -> Result {
+            let sender = ensure_signed(origin)?;
+            ensure!(<Kitties<T>>::exists(kitty_id), "Kitty doesn't exist");
+
+            let owner = Self::owner_of(kitty_id).ok_or("No owner for this object")?;
+            ensure!(owner == sender, "sender is not the owner");
+
+            let mut kitty = Self::kitty(kitty_id);
+            kitty.price = new_price;
+            <Kitties<T>>::insert(kitty_id, kitty);
+            Self::deposit_event(RawEvent::PriceSet(owner, kitty_id, new_price));
             Ok(())
         }
     }
@@ -76,15 +89,17 @@ decl_module! {
 
 impl<T: Trait> Module<T> {
     fn mint(to: T::AccountId, kitty_id: T::Hash, new_kitty: Kitty<T::Hash, T::Balance>) -> Result {
-        let owned_kitty_count = Self::kitties_count_by_owner(&to);
+        ensure!(!<KittyOwner<T>>::exists(kitty_id), "Kitty already exists");
+
+        let owned_kitty_count = Self::owned_kitty_count(&to);
+
         let new_owned_kitty_count = owned_kitty_count.checked_add(1)
             .ok_or("Overflow adding a new kitty to account balance")?;
 
         let all_kitties_count = Self::all_kitties_count();
+
         let new_all_kitties_count = all_kitties_count.checked_add(1)
             .ok_or("Overflow adding a new kitty to total supply")?;
-
-        ensure!(!<KittyOwner<T>>::exists(kitty_id), "Kitty already exists");
 
         <Kitties<T>>::insert(kitty_id, new_kitty);
         <KittyOwner<T>>::insert(kitty_id, &to);
@@ -96,7 +111,8 @@ impl<T: Trait> Module<T> {
         <OwnedKittiesArray<T>>::insert((to.clone(), owned_kitty_count), kitty_id);
         <OwnedKittiesCount<T>>::insert(&to, new_owned_kitty_count);
         <OwnedKittiesIndex<T>>::insert(kitty_id, owned_kitty_count);
-        Self::deposit_event(RawEvent::CreatedEvent(to, kitty_id));
+
+        Self::deposit_event(RawEvent::Created(to, kitty_id));
 
         Ok(())
     }
